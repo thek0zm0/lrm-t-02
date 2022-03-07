cd838f118a2c8bb9692ea1c7ea7744fb454f8b7d
LucasRM <lucasmoraes@alunos.utfpr.edu.br>
2022-03-07 13:58:55 -0300

    Implementando Exceções, validações e segurança

--- src/main/java/hfoods/demo/entities/User.java
+++ src/main/java/hfoods/demo/entities/User.java
@@ -1,13 +1,14 @@
 package hfoods.demo.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,7 +17,7 @@ import java.util.Set;
 @EqualsAndHashCode
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     private static final long serialVersionUID = 9178661439383356177L;
     @Id
@@ -41,4 +42,35 @@ public class User implements Serializable {
     @ManyToOne
     @JoinColumn(name = "diet_id")
     private Diet diet;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
