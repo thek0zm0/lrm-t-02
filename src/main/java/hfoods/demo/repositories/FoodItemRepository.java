5ba46ff82b08bc92ac2e3a1380b4ec1bc716e969
LucasRM <lucasmoraes@alunos.utfpr.edu.br>
2022-03-07 08:33:22 -0300

    FoodItem Repository corrigido

--- src/main/java/hfoods/demo/repositories/FoodItemRepository.java
+++ src/main/java/hfoods/demo/repositories/FoodItemRepository.java
@@ -1,7 +1,8 @@
 package hfoods.demo.repositories;
 
 import hfoods.demo.entities.FoodItem;
+import hfoods.demo.entities.pk.FoodItemPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
+public interface FoodItemRepository extends JpaRepository<FoodItem, FoodItemPk> {
 }
