79e8a30e36e386fcac1a387722f5fdc6298be50a
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2022-11-03 17:00:17 -0300

    Atualiza configurações backend

--- src/main/java/hfoods/demo/config/WebSecurityConfig.java
+++ src/main/java/hfoods/demo/config/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/actuator/**");
+		web.ignoring().antMatchers("/h2-console/**");
 	}
 
 	@Override
